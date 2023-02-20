d690b61c12bb0d4af9d9bab5f0da952c022c0f5d
brennuck <brennuck@gmail.com>
2023-02-20 12:27:03 -0700

    update

--- joDatabase/mongo.js
+++ joDatabase/mongo.js
@@ -1,13 +1,17 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-    "mongodb+srv://brennuck:console.log(Hi2933)@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function main() {
+    const uri = `mongodb+srv://brennuck:${process.env.MONGODB_PASSWORD}@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1,
+    });
+    client.connect((err) => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection object
+        client.close();
+    });
+}
+
+main().catch(console.error);
