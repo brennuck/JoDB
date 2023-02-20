742cb14f7bb775bf43b363e520f41099315f3ef7
brennuck <brennuck@gmail.com>
2023-02-20 13:23:16 -0700

    data

--- joDatabase/mongo.js
+++ joDatabase/mongo.js
@@ -1,17 +1,25 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 async function main() {
-    const uri = `mongodb+srv://brennuck:${process.env.MONGODB_PASSWORD}@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
+    console.log("process.env", process.env.MONGODB_PASSWORD);
+    const uri = `mongodb+srv://brennuck:console.log(Hi2933)@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1,
     });
-    client.connect((err) => {
-        const collection = client.db("test").collection("devices");
-        // perform actions on the collection object
-        client.close();
-    });
+    await client.connect();
+
+    const cursor = client.db("prod_updates").collection("updates").find();
+
+    const results = await cursor.toArray();
+    if (results.length > 0) {
+        console.log(`Found ${results.length} listing(s):`);
+        results.forEach((result, i) => {
+            console.log("Date:", result.date);
+            console.log("Update:", result.update);
+            console.log("Color:", result.color);
+        });
+    }
+    client.close();
 }
 
 main().catch(console.error);
