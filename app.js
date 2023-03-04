57da8eb63fdcdaa5ddb197093d35919b4b458431
brennuck <brennuck@gmail.com>
2023-03-04 11:55:32 -0700

    adssd

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const { default: db } = require("./mongo.mjs");
-const app = express();
-
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import { db } from "./mongo.mjs";
 
+const app = express();
+
 app.use(express.static("public"));
-
-// const port = 3000;
-
-console.log("port", process.env.port);
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
--- mongo.mjs
+++ mongo.mjs
@@ -1,41 +1,5 @@
-// const { MongoClient } = require("mongodb");
-require("dotenv").config();
-
-// async function getUpdates() {
-//     console.log("process.env", process.env.MONGODB_PASSWORD);
-//     const uri = `mongodb+srv://brennuck:${process.env.MONGODB_PASSWORD}@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
-//     const client = new MongoClient(uri, {
-//         useNewUrlParser: true,
-//     });
-//     try {
-//         await client.connect();
-
-//         const cursor = client.db("prod_updates").collection("updates").find();
-
-//         // const docs = {
-//         //     date: new Date(),
-//         //     update: "Testing",
-//         //     color: "default",
-//         // };
-//         // const result = await cursor.insert(docs);
-
-//         // console.log("result ====>", result);
-
-//         const results = await cursor.toArray();
-//         if (results.length > 0) {
-//             console.log(`Found ${results.length} listing(s):`);
-//             results.forEach((result, i) => {
-//                 console.log("Date:", result.date);
-//                 console.log("Update:", result.update);
-//                 console.log("Color:", result.color);
-//             });
-//         }
-//     } finally {
-//         client.close();
-//     }
-// }
-
-// getUpdates().catch(console.error);
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import { MongoClient } from "mongodb";
 
@@ -50,6 +14,6 @@ try {
     console.error(e);
 }
 
-let db = conn.db("prod_updates");
+export let db = conn.db("prod_updates");
 
-export default db;
+// export default db;
