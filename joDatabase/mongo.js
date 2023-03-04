b47641d0588a0167c15dedfc6325fb2a5f7a450d
brennuck <brennuck@gmail.com>
2023-03-04 09:45:22 -0700

    updates

--- joDatabase/mongo.js
+++ joDatabase/mongo.js
@@ -1,25 +1,38 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-async function main() {
+async function getUpdates() {
     console.log("process.env", process.env.MONGODB_PASSWORD);
-    const uri = `mongodb+srv://brennuck:console.log(Hi2933)@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://brennuck:${process.env.MONGODB_PASSWORD}@journiedb.epkam7m.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, {
         useNewUrlParser: true,
     });
-    await client.connect();
+    try {
+        await client.connect();
 
-    const cursor = client.db("prod_updates").collection("updates").find();
+        const cursor = client.db("prod_updates").collection("updates").find();
 
-    const results = await cursor.toArray();
-    if (results.length > 0) {
-        console.log(`Found ${results.length} listing(s):`);
-        results.forEach((result, i) => {
-            console.log("Date:", result.date);
-            console.log("Update:", result.update);
-            console.log("Color:", result.color);
-        });
+        // const docs = {
+        //     date: new Date(),
+        //     update: "Testing",
+        //     color: "default",
+        // };
+        // const result = await cursor.insert(docs);
+
+        // console.log("result ====>", result);
+
+        const results = await cursor.toArray();
+        if (results.length > 0) {
+            console.log(`Found ${results.length} listing(s):`);
+            results.forEach((result, i) => {
+                console.log("Date:", result.date);
+                console.log("Update:", result.update);
+                console.log("Color:", result.color);
+            });
+        }
+    } finally {
+        client.close();
     }
-    client.close();
 }
 
-main().catch(console.error);
+getUpdates().catch(console.error);
