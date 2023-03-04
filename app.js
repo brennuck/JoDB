4e403cc6b2439b169c6428a31610a28df2534e95
brennuck <brennuck@gmail.com>
2023-03-04 11:13:19 -0700

    adasd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const port = 3000;
+// const port = 3000;
 
 console.log("port", process.env.port);
 
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(3000, () => {
-    console.log(`Example app listening on port ${3000}!`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Example app listening on port ${process.env.PORT || 5000}!`);
 });
