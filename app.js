b249608fd44f4a4c2b6aea6ae624aebf6895a904
brennuck <brennuck@gmail.com>
2023-03-04 10:19:39 -0700

    ads

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
+app.use(express.static("public"));
+
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(port, () => {
